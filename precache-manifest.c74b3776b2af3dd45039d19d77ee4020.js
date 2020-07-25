f61e5c84eaeb2b957d32c6bc6b1138b66d9bcdd2
Sonny-2k <Sonny-2k@users.noreply.github.com>
2020-07-25 15:09:16 +0700

    Updates

---
+++ precache-manifest.c74b3776b2af3dd45039d19d77ee4020.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea6658f27af352c825fd766d863c1e9",
+    "url": "/calculator-reactjs/index.html"
+  },
+  {
+    "revision": "db97e670a48569882b59",
+    "url": "/calculator-reactjs/static/css/main.e82ade11.chunk.css"
+  },
+  {
+    "revision": "7eb10fcaf8735ce4bfbf",
+    "url": "/calculator-reactjs/static/js/2.4427ddfd.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/calculator-reactjs/static/js/2.4427ddfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db97e670a48569882b59",
+    "url": "/calculator-reactjs/static/js/main.70fc2d85.chunk.js"
+  },
+  {
+    "revision": "c8cbea9c1327cf5da882",
+    "url": "/calculator-reactjs/static/js/runtime-main.64c79020.js"
+  }
+]);
